8deb9dcced76aea8cd94191df02b76cf30c43f70
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-09-08 18:42:52 -0500

    Create constants.ts

---
+++ lib/profileRegistry/constants.ts
@@ -0,0 +1,4 @@
+// lib/profileRegistry/constants.ts
+export const BASE_CHAIN_ID = 8453; // Base mainnet
+export const REGISTRY_ADDRESS = "0x4769667dC49A8E05018729108fD98521F4EbC53a";
+export const USDC_ADDRESS     = "0x833589fCD6EDb6E08f4c7C32D4f71b54bdA02913"; // Base USDC (6d)
