71a6ba589d0f8f3c86dadf8258560fcf1976d2b6
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-09-12 11:41:50 -0500

    Update SafeMedia.tsx

--- components/SafeMedia.tsx
+++ components/SafeMedia.tsx
@@ -1,4 +1,3 @@
-// components/SafeMedia.tsx
 'use client';
 
 type Props = {
@@ -17,10 +16,8 @@ function looksLike(exts: string[], url: string) {
 
 export default function SafeMedia({ src, className = '', rounded = 'rounded-xl' }: Props) {
   if (looksLike(IMAGE_EXT, src)) {
-    return (
-      // eslint-disable-next-line @next/next/no-img-element
-      <img src={src} alt="" className={`${rounded} w-full h-56 object-cover ${className}`} />
-    );
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={src} alt="" className={`${rounded} w-full h-56 object-cover ${className}`} />;
   }
   if (looksLike(VIDEO_EXT, src)) {
     return (
@@ -33,15 +30,17 @@ export default function SafeMedia({ src, className = '', rounded = 'rounded-xl' 
       />
     );
   }
-  // Fallback: just show a link
+  // Fallback: generic opener, do NOT show the URL string
   return (
     <a
       href={src}
       target="_blank"
       rel="noreferrer"
       className={`${rounded} block w-full h-56 bg-white/[0.03] grid place-items-center`}
+      aria-label="Open content"
+      title="Open content"
     >
-      <span className="text-xs text-slate-400 truncate px-3">{src}</span>
+      <span className="text-xs text-slate-400">Open content</span>
     </a>
   );
 }
