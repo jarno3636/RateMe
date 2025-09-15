1a9a730e546629a88ee64d1af5df02f9f4a1251a
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-09-15 13:03:44 -0500

    Delete app directory

--- app/creator/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/creator/page.tsx
-import CreatorOnboardClient from './CreatorOnboardClient';
-
-export const dynamic = 'force-dynamic';
-
-export default function CreatorPage() {
-  return <CreatorOnboardClient />;
-}
