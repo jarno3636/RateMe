14ba6673214efb82e05d358ae7afa248514fa07d
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-09-23 11:55:52 -0500

    Create FarcasterIcon.tsx

---
+++ components/icons/FarcasterIcon.tsx
@@ -0,0 +1,12 @@
+export function FarcasterIcon({ className = "h-5 w-5" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={className}
+    >
+      <path d="M128 0C57.307 0 0 57.307 0 128s57.307 128 128 128 128-57.307 128-128S198.693 0 128 0zm0 24c57.42 0 104 46.58 104 104s-46.58 104-104 104S24 185.42 24 128 70.58 24 128 24zm-5 48v112h22V72h-22zm-38 20v72h22V92H85zm76 0v72h22V92h-22z" />
+    </svg>
+  );
+}
