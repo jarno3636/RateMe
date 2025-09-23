94f86aeb1a2911fdf4aa5ee8b22f70bc6f95eebb
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-09-23 11:56:27 -0500

    Create XIcon.tsx

---
+++ components/icons/XIcon.tsx
@@ -0,0 +1,12 @@
+export function XIcon({ className = "h-5 w-5" }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.24l-5.355-7.007-6.13 7.007H1.446l7.73-8.835L.149 2.25H8.01l4.84 6.375 5.394-6.375z" />
+    </svg>
+  );
+}
